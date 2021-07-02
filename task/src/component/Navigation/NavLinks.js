3c83ecf49f1761ef90ea87d28966eae72caa5508
Nada <nisrinaseptiana026@gmail.com>
2021-07-02 18:29:44 +0700

    update logo, navllink

--- task/src/component/Navigation/NavLinks.js
+++ task/src/component/Navigation/NavLinks.js
@@ -2,19 +2,24 @@ import React from "react";
 
 import "../../assets/css/NavLinks.css";
 import { NavLink } from "react-router-dom";
+//assets
+import logoModuit from "../../assets/img/logo-moduit.svg";
 const NavLinks = () => {
   return (
     <ul className="nav-links">
       {
         <React.Fragment>
+          <li className="mb-2">
+            <img src={logoModuit} />
+          </li>
           <li>
             <NavLink exact to="/">
-              Task 1
+              Question 1
             </NavLink>
           </li>
           <li>
             <NavLink exact to="/task2">
-              Task 2
+              Question 2
             </NavLink>
           </li>
         </React.Fragment>
